78c3956219e7da2aa6e4c8c6c228aea8e92bbabb
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-08-12 14:56:24 +0000

    testing python module imports on wcoss2

---
+++ imports.py
@@ -0,0 +1,56 @@
+import argparse
+import csv
+import datetime
+from datetime import datetime, timedelta
+from ftplib import FTP
+import glob
+import math
+from math import *
+import os
+import pstats
+import sys 
+import urllib.request
+
+
+import matplotlib
+import matplotlib.colors as colors
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+import scipy
+from scipy.ndimage.filters import gaussian_filter
+
+from netCDF4 import Dataset
+from netCDF4 import Dataset,  num2date
+import netCDF4
+
+from PIL import Image
+from shutil import copy2
+
+import numpy
+import numpy as np
+import numpy.ma as ma
+
+import pyproj
+import shutil 
+import tarfile
+
+import ncepgrib2
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+
+exit(0)
+
+#import pygrib -- replaced/substituted by ncepgrib2
+
+import networkx as netx
+
+from docx import Document
+from docx.shared import Inches
+from pptx import Presentation
+
+import mpl_toolkits.basemap as basemap
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import shiftgrid
---
+++ imports.sh
@@ -0,0 +1,8 @@
+
+module load intel/19.1.3.304
+module load python/3.8.6
+module load proj/7.1.0
+module load geos/3.8.1
+
+python3 imports.py 
+
