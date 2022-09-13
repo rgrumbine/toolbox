1f7dd8fe55d810c4f5e9ba3faa413ae439335005
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-09-13 12:27:07 +0000

    dates in reget

--- dataflow/reget.2yr.sh
+++ dataflow/reget.2yr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
-#PBS -N reget2
-#PBS -o reget2
+#PBS -N reget4
+#PBS -o reget4
 #PBS -j oe
 #PBS -A XFER-DEV
 #PBS -q dev_transfer
@@ -8,12 +8,6 @@
 #PBS -l select=1:ncpus=1
 
 set -x
-
-#module load prod_envir/1.0.2
-#module load ips/18.0.1.163
-#module load prod_util/1.1.0
-#module load HPSS/5.0.2.5
-
 
 ssmi_file=./b012/xx001
 amsre_file=./b021/xx254
@@ -23,13 +17,13 @@ ssmisu_file=./b021/xx201
 set -x
 # Span back to 'dawn of time'
 #name change 26 Feb 2020
-end_date=20220524
+end_date=20220101
 start_date=`date +"%Y%m%d"`
 start_date=`expr $start_date - 1`
 start_date=`$HOME/bin/dtgfix3 $start_date`
 start_date=`expr $start_date - 1`
 start_date=`$HOME/bin/dtgfix3 $start_date`
-start_date=20220624
+start_date=20220626
 
 base=$HOME/noscrub/satellites
 if [ ! -d $base ] ; then
