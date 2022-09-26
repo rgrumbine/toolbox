491969924e63d1c9200099e5bcb6fe5547c6a415
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-09-26 18:14:51 +0000

    fix for changing dcom history names

--- dataflow/reget.2yr.sh
+++ dataflow/reget.2yr.sh
@@ -1,13 +1,11 @@
 #!/bin/bash --login
-#PBS -N reget5
-#PBS -o reget5
+#PBS -N reget22b
+#PBS -o reget22b
 #PBS -j oe
 #PBS -A XFER-DEV
 #PBS -q dev_transfer
 #PBS -l walltime=6:00:00
 #PBS -l select=1:ncpus=1
-
-set -x
 
 ssmi_file=./b012/xx001
 amsre_file=./b021/xx254
@@ -19,27 +17,31 @@ set -x
 #name change 26 Feb 2020
 #  what is now dcom_prod was dcom_us007003
 
-end_date=20190101
+end_date=20180101
 start_date=`date +"%Y%m%d"`
 start_date=`expr $start_date - 1`
 start_date=`$HOME/bin/dtgfix3 $start_date`
 start_date=`expr $start_date - 1`
 start_date=`$HOME/bin/dtgfix3 $start_date`
-start_date=20190626
+#start_date=20211231
 
 base=$HOME/noscrub/satellites
 if [ ! -d $base ] ; then
   mkdir -p $base
 fi
 
+set -x
+
 date=$start_date
 while [ $date -ge $end_date ]; do
 
   dcom_dir=${base}/prod/$date
   if [ $date -le 20200226 ] ; then
-	  export dtag=us007003
+	  export dtag=us007003_$date
+  elif [ $date -le 20220626 ] ; then
+	  export dtag=prod_$date
   else
-	  export dtag=prod
+	  export dtag=$date
   fi
 
   if [ ! -d $dcom_dir ] ; then 
@@ -50,10 +52,10 @@ while [ $date -ge $end_date ]; do
     yr=`echo $date | cut -c1-4`
     yrmo=`echo $date | cut -c1-6`
   
-    hpss_dcom=/NCEPPROD/hpssprod/runhistory/rh${yr}/${yrmo}/${date}/dcom_${dtag}_${date}.tar
+    hpss_dcom=/NCEPPROD/hpssprod/runhistory/rh${yr}/${yrmo}/${date}/dcom_${dtag}.tar
 
     cd $dcom_dir
-    date
+    #date
     set -x
     #/nwprod/util/ush/hpsstar getnostage $hpss_dcom $ssmi_file $amsr2_file $amsre_file $ssmisu_file
     htar -xf $hpss_dcom $ssmi_file $amsr2_file $amsre_file $ssmisu_file
