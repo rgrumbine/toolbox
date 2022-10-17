9b1e6ec59293515d5a8604e9cbcf33658fddd904
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-10-17 18:22:22 +0000

    adding iabp retrievals

--- dataflow/days_data.sh
+++ dataflow/days_data.sh
@@ -6,7 +6,9 @@
 time ./cislakes.sh #-- kb/wk
 time ./nic_edge.sh #-- kb/day
 
-#iabp? -- $DCOM/prod/...
+#iabp
+time ./iabp_dcomdev #-- 500 Mb/day
+time ./iabp.wget    #-- can be up to a few Gb, but updates only monthly
 
 time ./osisaf      # --  76 Mb/day
 time ./get_cafs.sh # -- 500 Mb/day
@@ -26,3 +28,5 @@ time   ./gefs_thin.sh   # this winds up cleaning yesterday and before,
 time ./rtofs_cice_copy.sh # -- 27 Gb/day
 time ./giops.sh           # -- 28 Gb/day 
 #viirs -- 32 Gb/day
+-rwxr-xr-x 1 robert.grumbine couple 398 Oct 17 17:57 iabp_dcomdev
+-rwxr-xr-x 1 robert.grumbine emc    460 Oct 17 17:51 iabp.wget
