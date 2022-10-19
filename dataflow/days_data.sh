701f06d1d0b738e5da33b7754ebc624313c339ed
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-10-19 15:38:00 +0000

    ims iabp updates

--- dataflow/days_data.sh
+++ dataflow/days_data.sh
@@ -11,9 +11,10 @@ time ./iabp_dcomdev #-- 500 Mb/day
 time ./iabp.wget    #-- can be up to a few Gb, but updates only monthly
 
 time ./osisaf      # --  76 Mb/day
-time ./get_cafs.sh # -- 500 Mb/day
+time ./ims.wget    # -- 0.5 Mb/day, but may get several months at a time
 time ./nsidc       # updates only about quarterly
 
+time ./get_cafs.sh # -- 500 Mb/day
 #submit jobs that reference hpss to q so that data transfer nodes+queues can be used
 qsub ./reget.2yr.sh # -- 2Gb/day
 
@@ -21,7 +22,6 @@ qsub ./gefs_get.sh   #  ./gefs_thin.sh, ./gefs_clean.sh -- 1 Gb/day
 time   ./gefs_thin.sh   # this winds up cleaning yesterday and before, 
                         # since the queue won't respond instantly
 #sice
-#ims
 
 #While large volumes, these are disk to disk copy from com to my space
 time ./rtofs_cice_copy.sh # -- 27 Gb/day
