19de6ecb40a95f53116804baa93c53220d64d4e3
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-11-28 13:44:17 +0000

    python graphics

--- plot_cice/coasts.py
+++ plot_cice/coasts.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 import cartopy.crs as ccrs
 
-#matplotlib.use('agg')
+matplotlib.use('agg') #non-interactive
 #-------------------------------------------
 
 projection = ccrs.Miller()
@@ -15,6 +15,6 @@ print("about to call coastlines",flush=True)
 #ax.coastlines(resolution = '110m')
 ax.coastlines()
 print("back from call coastlines",flush=True)
-#plt.savefig("hello2.png")
-plt.show()
+plt.savefig("hello2.png")
+#plt.show()
 print("done",flush=True)
