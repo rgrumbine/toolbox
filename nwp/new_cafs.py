bdeaba62c5b25b45999bb1bb59d32d1647098d85
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2024-10-17 13:28:34 +0000

    run over all lead times

--- nwp/new_cafs.py
+++ nwp/new_cafs.py
@@ -11,11 +11,12 @@ from netCDF4 import Dataset
 
 import networkx as netx
 
+# User-written
 from functions import *
 from graphics import *
 
 #--------------------------------------------------------
-base = "/home/Robert.Grumbine/clim_data/cafs/"
+base = os.environ['base']
 
 #tag = datetime.datetime(2022,4,1)
 #debug: print("args ",sys.argv, flush=True)
@@ -23,11 +24,11 @@ tag = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]) )
 
 dname = tag.strftime("%Y%m%d")
 fname = "REB2."+tag.strftime("%Y-%m-%d") + ".nc"
-if (not os.path.exists(base+dname+"/"+fname) ):
-  print("could not open ",base+dname+"/"+fname)
+if (not os.path.exists(base+'/'+dname+"/"+fname) ):
+  print("could not open ",base+'/'+dname+"/"+fname)
   exit(1)
 
-fin = Dataset(base+dname+"/"+fname, "r")
+fin = Dataset(base+'/'+dname+"/"+fname, "r")
 nx = len(fin.dimensions["ni"])
 ny = len(fin.dimensions["nj"])
 lons = fin.variables["TLON"][:,:]
@@ -35,8 +36,8 @@ lats = fin.variables["TLAT"][:,:]
 tarea = fin.variables["tarea"][:,:]
 
 
-#for tstep in range(0,40):
-for tstep in range(0,1):
+for tstep in range(0,40):
+#debug: for tstep in range(0,2):
   aice = fin.variables["aice_h"][tstep,:,:]
   hs   = fin.variables["hs_h"][tstep,:,:]
   hi   = fin.variables["hi_h"][tstep,:,:]
@@ -66,174 +67,176 @@ for tstep in range(0,1):
     i = indices[1][k]
     land[j,i] = 0.0 
 
-#--------------------------------------------------------
-# define a subset to search within for paths
-latmin = 64.0
-latmax = 80.0
-lonmin = 185.0
-lonmax = 290.0
+  #--------------------------------------------------------
+  # define a subset to search within for paths
+  latmin = 64.0
+  latmax = 80.0
+  lonmin = 185.0
+  lonmax = 290.0
 
-# Construct nodes -- brute force looping over all grid points:
-nodemap = np.zeros((ny,nx),dtype="int")
-
-k = int(1)
-#Not a directed graph
-G = netx.Graph()
-
-#1 -> steps in i,j space
-#2 -> meters
-#3 -> weighted by polar class
-#4 -> weight by 1./(1.1-aice)
-cost_type = 4
-
-for i in range(0,nx):
-  for j in range(0,ny):
-    #debug: if (k%1000 == 0):
-      #debug: print("adding nodes, i = ",i, flush=True)
-    if (lats[j,i] > latmin and lats[j,i] < latmax and
-        lons[j,i] > lonmin and lons[j,i] < lonmax     ):
-      if (land[j,i] == 0):
-        nodemap[j,i] = int(k)
-        G.add_node(k, i = i, j =j, lat = lats[j,i], lon = lons[j,i],  aice=aice[j,i], hi = hi[j,i])
-        k += int(1)
-#debug: print("Done adding nodes, k=",k, flush=True)
-
-# Construct edges between nodes
-k = 0
-for j in range(0,ny):
-  jp = j + 1
-  jm = j - 1
+  # Construct nodes -- brute force looping over all grid points:
+  nodemap = np.zeros((ny,nx),dtype="int")
+  
+  k = int(1)
+  #Not a directed graph
+  G = netx.Graph()
+  
+  #1 -> steps in i,j space
+  #2 -> meters
+  #3 -> weighted by polar class
+  #4 -> weight by 1./(1.1-aice)
+  cost_type = 4
+  
   for i in range(0,nx):
-    ip = i+1
-    im = i-1
-    if (not (lats[j,i] > latmin and lats[j,i] < latmax and
-             lons[j,i] > lonmin and lons[j,i] < lonmax     )    ):
-      continue
-    n = nodemap[j,i]
-    if (n == 0):
-      continue
-
-    if (im >= 0):
-      if (nodemap[j,im] != 0):
-        weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[j,im], lon2 = lons[j,im], aice = aice[j,i])
-        G.add_edge(n, nodemap[j,im], weight= weight)
-        k += 1
-
-    if (ip < nx):
-      if (nodemap[j,ip] != 0):
-        weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[j,ip], lon2 = lons[j,ip], aice = aice[j,i])
-        G.add_edge(n, nodemap[j,ip], weight = weight)
-        k += 1
-
-    if (jp < ny ):
-      if (nodemap[jp,i] != 0):
-        weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,i], lon2 = lons[jp,i], aice = aice[j,i])
-        G.add_edge(n, nodemap[jp,i], weight = weight)
-        k += 1
+    for j in range(0,ny):
+      #debug: if (k%1000 == 0):
+        #debug: print("adding nodes, i = ",i, flush=True)
+      if (lats[j,i] > latmin and lats[j,i] < latmax and
+          lons[j,i] > lonmin and lons[j,i] < lonmax     ):
+        if (land[j,i] == 0):
+          nodemap[j,i] = int(k)
+          G.add_node(k, i = i, j =j, lat = lats[j,i], lon = lons[j,i],  aice=aice[j,i], hi = hi[j,i])
+          k += int(1)
+  #debug: print("Done adding nodes, k=",k, flush=True)
+  
+  # Construct edges between nodes
+  k = 0
+  for j in range(0,ny):
+    jp = j + 1
+    jm = j - 1
+    for i in range(0,nx):
+      ip = i+1
+      im = i-1
+      if (not (lats[j,i] > latmin and lats[j,i] < latmax and
+               lons[j,i] > lonmin and lons[j,i] < lonmax     )    ):
+        continue
+      n = nodemap[j,i]
+      if (n == 0):
+        continue
+  
       if (im >= 0):
-        if (nodemap[jp,im] != 0):
-          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,im], lon2 = lons[jp,im], aice = aice[j,i])
-          G.add_edge(n, nodemap[jp,im], weight = weight)
+        if (nodemap[j,im] != 0):
+          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[j,im], lon2 = lons[j,im], aice = aice[j,i])
+          G.add_edge(n, nodemap[j,im], weight= weight)
           k += 1
+  
       if (ip < nx):
-        if (nodemap[jp,ip] != 0):
-          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,ip], lon2 = lons[jp,ip], aice = aice[j,i])
-          G.add_edge(n, nodemap[jp,ip], weight = weight)
+        if (nodemap[j,ip] != 0):
+          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[j,ip], lon2 = lons[j,ip], aice = aice[j,i])
+          G.add_edge(n, nodemap[j,ip], weight = weight)
           k += 1
-
-    if (jm >= 0 ):
-      if (nodemap[jm,i] != 0):
-        weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,i], lon2 = lons[jm,i], aice = aice[j,i])
-        G.add_edge(n, nodemap[jm,i], weight = weight)
-        k += 1
-      if (im >= 0):
-        if (nodemap[jm,im] != 0):
-          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,im], lon2 = lons[jm,im], aice = aice[j,i])
-          G.add_edge(n, nodemap[jm,im], weight = weight)
+  
+      if (jp < ny ):
+        if (nodemap[jp,i] != 0):
+          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,i], lon2 = lons[jp,i], aice = aice[j,i])
+          G.add_edge(n, nodemap[jp,i], weight = weight)
           k += 1
-      if (ip < nx):
-        if (nodemap[jm,ip] != 0):
-          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,ip], lon2 = lons[jm,ip], aice = aice[j,i])
-          G.add_edge(n, nodemap[jm,ip], weight = weight)
+        if (im >= 0):
+          if (nodemap[jp,im] != 0):
+            weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,im], lon2 = lons[jp,im], aice = aice[j,i])
+            G.add_edge(n, nodemap[jp,im], weight = weight)
+            k += 1
+        if (ip < nx):
+          if (nodemap[jp,ip] != 0):
+            weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jp,ip], lon2 = lons[jp,ip], aice = aice[j,i])
+            G.add_edge(n, nodemap[jp,ip], weight = weight)
+            k += 1
+  
+      if (jm >= 0 ):
+        if (nodemap[jm,i] != 0):
+          weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,i], lon2 = lons[jm,i], aice = aice[j,i])
+          G.add_edge(n, nodemap[jm,i], weight = weight)
           k += 1
+        if (im >= 0):
+          if (nodemap[jm,im] != 0):
+            weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,im], lon2 = lons[jm,im], aice = aice[j,i])
+            G.add_edge(n, nodemap[jm,im], weight = weight)
+            k += 1
+        if (ip < nx):
+          if (nodemap[jm,ip] != 0):
+            weight = cost(cost_type, lat1 = lats[j,i], lon1 = lons[j,i], lat2 = lats[jm,ip], lon2 = lons[jm,ip], aice = aice[j,i])
+            G.add_edge(n, nodemap[jm,ip], weight = weight)
+            k += 1
 
-#debug: print("Have constructed graph, number of edges =",k, len(G.edges), flush=True)
-#debug: exit(0)
-
-#--------------------------------------------------------
-#    Select start and finish points
-
-##start in Bering strait
-##Lat = 65.68, Lon = -168.59
-#i_start = int(0.5 + (360. - 168.59)*12.)
-#j_start = int(0.5 + (90.0 -  65.68)*12.)
-##finish in ... Baffin Bay
-##Lat = 74.0 N, -78.0
-#i_finish = int(0.5 + (360. - 78.0)*12.)
-#j_finish = int(0.5 + (90.0 - 74.0)*12.)
-
-#for j in range(0,ny):
-#  for i in range(0, nx):
-#      print(i, j, nodemap[j,i])
-# nodemap = 0 when i,j not in map
-
-#exit(0)
-
-# cafs grid is different
-#RG: Need a findij function (65.68 N, -168.59), (64.0, -78.0) 
-#RG: is baffin bay insize cafs domain?
-i_start = int(287)
-j_start = int(67)
-#i_finish = int(415)
-#j_finish = int(420)
-i_finish = int(396) 
-j_finish = int(344) 
-#debug: print("start: ",lons[j_start, i_start], lats[j_start, i_start] )
-#debug: print("finis: ",lons[j_finish, i_finish], lats[j_finish, i_finish] )
-
-# Quick check to see whether there are _any_ paths:
-start  = nodemap[j_start, i_start]
-finish = nodemap[j_finish, i_finish]
-#debug: print("start node ",G.nodes[start])
-#debug: print("finish node ",G.nodes[finish])
-#debug: print(i_start, j_start, i_finish, j_finish, start, finish)
-print("Is there a path from start to finish? ",netx.has_path(G,start,finish ), flush=True )
-if (not netx.has_path(G,start,finish )):
-  exit(1)
-
-#------------------------------------------------
-path = netx.dijkstra_path(G,start, finish)
-print("dijkstra length and score ", len(path), 
-       netx.dijkstra_path_length(G, start, finish), flush=True)
-pseudo_length = netx.dijkstra_path_length(G, start, finish)
-
-for k in range(0,len(path)):
-  print(k,G.nodes[path[k]])
-  #print(k,
-  #      G.nodes[path[k]]['i'], G.nodes[path[k]]['j'],
-  #      G.nodes[path[k]]['lon'], G.nodes[path[k]]['lat'],
-  #      G.nodes[path[k]]['aice'], G.nodes[path[k]]['hi'],
-  #      flush=True )
-print("",flush=True)
-tlons = np.zeros((len(path)))
-tlats = np.zeros((len(path)))
-
-kmlout = open("path_"+tag.strftime("%Y%m%d")+"_00.kml","w")
-#RG: Need kml header and footer information
-for k in range(0,len(path)):
-#  if (G.nodes[path[k]]['lon'] > 180.):
-#    tlon = G.nodes[path[k]]['lon']  - 360.
-#    tlons[k] = tlon
-#  else:
-  tlon = G.nodes[path[k]]['lon']
-  tlons[k] = tlon
-  tlats[k] = G.nodes[path[k]]['lat']
-  print("<Placemark> <Point> <coordinates>",tlon,G.nodes[path[k]]['lat'],0.0,
-        "</coordinates></Point></Placemark>", file=kmlout)
-
+  #debug: print("Have constructed graph, number of edges =",k, len(G.edges), flush=True)
   #debug: exit(0)
+
+  #--------------------------------------------------------
+  #    Select start and finish points
+  
+  ##start in Bering strait
+  ##Lat = 65.68, Lon = -168.59
+  #i_start = int(0.5 + (360. - 168.59)*12.)
+  #j_start = int(0.5 + (90.0 -  65.68)*12.)
+  ##finish in ... Baffin Bay
+  ##Lat = 74.0 N, -78.0
+  #i_finish = int(0.5 + (360. - 78.0)*12.)
+  #j_finish = int(0.5 + (90.0 - 74.0)*12.)
+  
+  #for j in range(0,ny):
+  #  for i in range(0, nx):
+  #      print(i, j, nodemap[j,i])
+  # nodemap = 0 when i,j not in map
+  
+  #exit(0)
+  
+  # cafs grid is different
+  #RG: Need a findij function (65.68 N, -168.59), (64.0, -78.0) 
+  #RG: is baffin bay insize cafs domain?
+  i_start = int(287)
+  j_start = int(67)
+  #i_finish = int(415)
+  #j_finish = int(420)
+  i_finish = int(396) 
+  j_finish = int(344) 
+  #debug: print("start: ",lons[j_start, i_start], lats[j_start, i_start] )
+  #debug: print("finis: ",lons[j_finish, i_finish], lats[j_finish, i_finish] )
+  
+  # Quick check to see whether there are _any_ paths:
+  start  = nodemap[j_start, i_start]
+  finish = nodemap[j_finish, i_finish]
+  #debug: print("start node ",G.nodes[start])
+  #debug: print("finish node ",G.nodes[finish])
+  #debug: print(i_start, j_start, i_finish, j_finish, start, finish)
+  print("Is there a path from start to finish? ",netx.has_path(G,start,finish ), flush=True )
+  if (not netx.has_path(G,start,finish )):
+    exit(1)
+
+  #------------------------------------------------
+  path = netx.dijkstra_path(G,start, finish)
+  print("dijkstra length and score ", len(path), 
+         netx.dijkstra_path_length(G, start, finish), flush=True)
+  pseudo_length = netx.dijkstra_path_length(G, start, finish)
+  
+  for k in range(0,len(path)):
+    print(k,G.nodes[path[k]])
+    #print(k,
+    #      G.nodes[path[k]]['i'], G.nodes[path[k]]['j'],
+    #      G.nodes[path[k]]['lon'], G.nodes[path[k]]['lat'],
+    #      G.nodes[path[k]]['aice'], G.nodes[path[k]]['hi'],
+    #      flush=True )
+  print("",flush=True)
+  tlons = np.zeros((len(path)))
+  tlats = np.zeros((len(path)))
+  
+  kmlout = open("path_"+tag.strftime("%Y%m%d")+"_"+"{:d}".format((tstep+1)*6)+".kml","w")
+  #RG: Need kml header and footer information
+  for k in range(0,len(path)):
+  #  if (G.nodes[path[k]]['lon'] > 180.):
+  #    tlon = G.nodes[path[k]]['lon']  - 360.
+  #    tlons[k] = tlon
+  #  else:
+    tlon = G.nodes[path[k]]['lon']
+    tlons[k] = tlon
+    tlats[k] = G.nodes[path[k]]['lat']
+    print("<Placemark> <Point> <coordinates>",tlon,G.nodes[path[k]]['lat'],0.0,
+          "</coordinates></Point></Placemark>", file=kmlout)
+  
+    #debug: exit(0)
+  
+#----------- Graphics ---------------------------------
+  show(tlats, tlons, tag, hours = (tstep+1)*6, cost = pseudo_length)
 #-----------------------------------------------------
-show(tlats, tlons, tag, cost = pseudo_length)
 
 
 """
--- nwp/runup.sh
+++ nwp/runup.sh
@@ -11,44 +11,45 @@
 #SBATCH --mail-type FAIL
 #SBATCH --mail-user USER@system
 
-# RG: enable sbatch submission
+# Path to cafs output:
+export base=/home/Robert.Grumbine/clim_data/cafs/
 
-# RG: location of python support
+# location of python and support
 cd /home/Robert.Grumbine/rgdev/toolbox/nwp
 
 # This must be more or less exactly this:
 source /home/Robert.Grumbine/rgref/env3.12/bin/activate
-
-# Path to cafs is currently hard-wired in the python program (boo!) RG
-export base=/home/Robert.Grumbine/clim_data/cafs/
 
 #These can be anything of convenience
 export MPLCONFIGDIR=$HOME/rgexpt/
 export OUTDIR=$HOME/rgdev/cafs_nwp
 
 #------------------------------------------------------
-tag=20220401
+#tag=20220401
+tag=20241015
 #debug: tag=20241001
 
+#reverse -- now to past
 end=`date +"%Y%m%d"`
+end=20220401
 #debug: end=20241011
 
-while [ $tag -le $end ]
+while [ $tag -ge $end ]
 do
 
   yy=`echo $tag | cut -c1-4`
   mm=`echo $tag | cut -c5-6`
   dd=`echo $tag | cut -c7-8`
-  #if [ ! -f $HOME/rgdev/cafs_nwp/out.$tag ] ; then
+  if [ ! -f $HOME/rgdev/cafs_nwp/out.$tag ] ; then
     time python3 new_cafs.py $yy $mm $dd > $OUTDIR/out.$tag
-  #fi
-  if [ -f nwp_${tag}_00.png ] ; then
-    mv nwp_${tag}_00.png $OUTDIR
+  fi
+  if [ -f nwp_${tag}_6.png ] ; then
+    mv nwp_${tag}_*.png $OUTDIR
   fi  
-  if [ -f path_${tag}_00.kml ] ; then
-    mv path_${tag}_00.kml $OUTDIR
+  if [ -f path_${tag}_6.kml ] ; then
+    mv path_${tag}_*.kml $OUTDIR
   fi  
 
-  tag=`expr $tag + 1`
+  tag=`expr $tag - 1`
   tag=`$HOME/bin/dtgfix3 $tag`
 done
