1ae1ca29157239a1f0babe8085078b6b7be6ba15
Robert Grumbine <Robert.Grumbine@noaa.gov>
2025-05-30 13:10:20 +0000

    support reruns of analysis

--- dataflow/sice.reget.sh
+++ dataflow/sice.reget.sh
@@ -16,13 +16,12 @@ end_date=20250101
 start_date=`date +"%Y%m%d"`
 ##start_date=`sh finddate.sh $start_date d-1`
 ##start_date=`sh finddate.sh $start_date d-1`
+#start_date=`expr $start_date - 1`
+#start_date=`$HOME/bin/dtgfix3 $start_date`
+#start_date=`expr $start_date - 1`
+#start_date=`$HOME/bin/dtgfix3 $start_date`
 
-start_date=`expr $start_date - 1`
-start_date=`$HOME/bin/dtgfix3 $start_date`
-start_date=`expr $start_date - 1`
-start_date=`$HOME/bin/dtgfix3 $start_date`
-
-#start_date=20210831
+start_date=20250311
 
 base=$HOME/noscrub/sice/
 if [ ! -d $base ] ; then
