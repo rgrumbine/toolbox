fix bayes computation

cluster/decision3.py
@@ -57,9 +57,9 @@ def bayes(tree, dr, ary, preds, pices, count):
         pices[ileaf] = 0.
       else:
         pice   = totice/count
-        pices[ileaf] = pice
         pclass = tot/count
-        pice_given_class = count11/tot
+        pice_given_class = (count01+count11)/tot
+        pices[ileaf] = pice_given_class
 
         if (pice == 0):
           pclass_given_ice = 0
@@ -74,7 +74,7 @@ def bayes(tree, dr, ary, preds, pices, count):
 #--------------------------------------------------------------------------
 countmax = 14123456
 fmax = int(countmax / 5)
-thin = 20
+thin = 32
 print("thin = ",thin)
 ice = 14 
 # ndarray to save for use in finding clusters
@@ -86,10 +86,13 @@ from reader import *
 count = 0
 dt = datetime.timedelta(1)
 end = datetime.datetime(2025,6,14)
-for mm in (1,2,3,4,5,6):
-  tag = datetime.datetime(2025,mm,2)
+tag = datetime.datetime(2025,1,2)
+#for mm in (1,2,3,4,5,6):
+#  tag = datetime.datetime(2025,mm,2)
+while (tag <= end):
   print(tag, flush=True)
   count = read(tag, ary, dr, count, fmax = fmax, countmax = countmax, thin = thin)
+  tag += dt
 
 count = min(count, countmax)
 print(count, " points to consider v countmax",countmax, flush=True)
@@ -99,9 +102,9 @@ x = ary[:count,ice]*100
 y = x.astype(dtype=np.int32)
 # can actually run with just the percents as targets, but start with ice binary
 # binary
-#for i in range(0,count):
-#   if (y[i] > 0):
-#       y[i] = 1
+for i in range(0,count):
+   if (y[i] > 0):
+       y[i] = 1
 # wavewatch critical levels
 #for i in range(0,count):
 #  if (y[i] > 70):
@@ -125,7 +128,7 @@ for depth in range(1,3):
   # want to do this only once; it's in bayes right now
   leaf = tree.apply(dr[:count])
 
-  t     = sklearn.tree.export_text(tree, max_depth = 7, decimals=3)
+  t     = sklearn.tree.export_text(tree, max_depth = 7, decimals = 3)
   print("\ndepth, leaves",tree.get_depth(), tree.get_n_leaves() )
   print("decision tree top for depth ",depth,"\n",t)
   importance = tree.feature_importances_
@@ -155,7 +158,7 @@ for depth in range(1,3):
 
   pice   = (count01 + count11)/count
   pclass = 1
-  pice_given_class = count11/count
+  pice_given_class = (count01+count11)/count
 
   if pice == 0:
     pclass_given_ice = 0

cluster/redecision.py
@@ -34,40 +34,43 @@ def bayes(tree, dr, ary, preds, pices, count):
       count01 = 0
       count10 = 0
       count11 = 0
+      totice  = 0
       for i in range (0, count):
+        if (ary[i,ice] > 0):
+            totice += 1
         if (leaf[i] == ileaf): 
           if (preds[i] == 0 and ary[i,ice] == 0):
               count00 += 1
-          elif (preds[i] > 0 and ary[i,ice] > 0):
+          if (preds[i] > 0 and ary[i,ice] > 0):
               count11 += 1
-          elif (preds[i] == 0 and ary[i,ice] > 0):
+          if (preds[i] == 0 and ary[i,ice] > 0):
               count01 += 1
-          elif (preds[i] > 0 and ary[i,ice] == 0):
+          if (preds[i] > 0 and ary[i,ice] == 0):
               count10 += 1
       tot    =  count00 + count01 + count10 + count11
       if (tot == 0):
         noop()
       elif (count01+count11 == 0):
-        print(ileaf, "no ice in leaf ",tot, tot/count)
+        print(ileaf, "no real ice in leaf ",tot, tot/count)
         pices[ileaf] = 0.
       else:
-        pice   = (count01 + count11)/tot
-        pices[ileaf] = pice
+        pice   = totice / count
         pclass = tot/count
-        if (count10+count11 == 0):
-          pice_given_class = 0
+        pice_given_class = (count01+count11)/tot
+        pices[ileaf] = pice_given_class
+        if (pice == 0):
+          pclass_given_ice = 0
         else:
-          pice_given_class = count11/(count10+count11)
-        pclass_given_ice = pice_given_class * pclass / pice
+          pclass_given_ice = pice_given_class * pclass / pice
         csi = count11/(count11 + count01 + count10)
         print(ileaf,"leaf", "{:.3f}".format(pice) , "{:.3f}".format(pclass) , 
-              "{:.3f}".format(pice_given_class) , "{:.3f}".format(pclass_given_ice), tot, tot/count,  "{:.3f}".format(pices[ileaf]), flush=True )
+              "{:.3f}".format(pice_given_class) , "{:.3f}".format(pclass_given_ice), tot, tot/count,  "{:.3f}".format(csi), flush=True )
 
 
 #--------------------------------------------------------------------------
 countmax = 14123456
 fmax = countmax
-#fmax = 10000
+fmax = 60000
 ice = 14
 # ndarray to save for use in finding clusters
 ary   = np.zeros((countmax,17))
@@ -103,7 +106,7 @@ y = x.astype(dtype=np.int32)
 import sklearn
 from sklearn.tree import DecisionTreeClassifier
 
-for depth in range(1,2):
+for depth in range(1,7):
   fout = open("fout."+"{:02d}".format(int(depth)), "w" )
   
   tree = DecisionTreeClassifier(max_depth = depth)
@@ -131,18 +134,18 @@ for depth in range(1,2):
   for i in range (0, count):
     if (preds[i] == 0 and ary[i,ice] == 0):
         count00 += 1
-    elif (preds[i] > 0 and ary[i,ice] > 0):
+    if (preds[i] > 0 and ary[i,ice] > 0):
         count11 += 1
-    elif (preds[i] == 0 and ary[i,ice] > 0):
+    if (preds[i] == 0 and ary[i,ice] > 0):
         count01 += 1
-    elif (preds[i] > 0 and ary[i,ice] == 0):
+    if (preds[i] > 0 and ary[i,ice] == 0):
         count10 += 1
 
   print("depth",depth, "tot%correct ",(count00+count11)/(count00 + count01 + count10 + count11))
 
   pice   = (count01 + count11)/count
-  pclass = (count10 + count11)/count
-  pice_given_class = count11/(count10+count11)
+  pclass = 1
+  pice_given_class = (count01+count11)/count
   pclass_given_ice = pice_given_class * pclass / pice
   csi = count11/(count11 + count01 + count10)
   print(depth,"totbayes", "{:.3f}".format(pice) , "{:.3f}".format(pclass) , "{:.3f}".format(pice_given_class) , "{:.3f}".format(pclass_given_ice), "{:.3f}".format(csi), flush=True )