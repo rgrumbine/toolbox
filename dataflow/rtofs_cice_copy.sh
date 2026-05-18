1050f07ef1a717af2d940c727fa2718ad48300e3
Robert Grumbine <Robert.Grumbine@noaa.gov>
2026-05-18 19:20:22 +0000

    update fallback dates

--- dataflow/rtofs_cice_copy.sh
+++ dataflow/rtofs_cice_copy.sh
@@ -31,8 +31,7 @@ echo zzz ops = $ops
 
 cd $ops
 base=$HOME/noscrub/model_intercompare/rtofs_cice/
-#tag=20250601
-tag=20260101
+tag=20260501
 end=`date +"%Y%m%d"`
 #end=`expr $end - 1`
 #end=`$HOME/bin/dtgfix3 $end`
