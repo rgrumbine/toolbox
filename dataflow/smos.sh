eb84684267f1c03d003abb1665c9a563d0c15ad7
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2026-05-26 16:13:25 +0000

    update smos

--- dataflow/smos.sh
+++ dataflow/smos.sh
@@ -4,10 +4,20 @@ module load prod_envir
 
 set -x
 tag=${tag:-`date +"%Y%m%d"`}
+k=0
 
-cd  /lfs/h1/ops/dev/dcom
-cd $tag
-cd seaice/smos-smap
-cp -p *.nc $HOME/noscrub/thickness/
+base=/lfs/h1/ops/dev/dcom
+outdir=$HOME/noscrub/thickness
 
+while [ $k -le 7 ] 
+do
+  if [ ! -f $outdir/${tag}_south_mix_sit_v300.nc ] ; then
+    cd $base/$tag
+    cd seaice/smos-smap
+    cp -p *.nc $outdir
+  fi
 
+  k=`expr $k + 1`
+  tag=`expr $tag - 1`
+  tag=`$HOME/bin/dtgfix3 $tag`
+done
