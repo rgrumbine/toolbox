e8e166cdfeb697fcfab13ed760b8eb971fe7d49d
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2026-05-26 13:52:35 +0000

    updating stacker notes and kd main_rtofs

--- kd/main_rtofs.f90
+++ kd/main_rtofs.f90
@@ -5,9 +5,12 @@ program test_global_map
     use kd_tree_mod
     implicit none
     ! RG:
-    INTEGER, parameter :: nx = 4500
-    INTEGER, parameter :: ny = 3298
+    !INTEGER, parameter :: nx = 4500
+    !INTEGER, parameter :: ny = 3298
+    INTEGER, parameter :: nx = 1440
+    INTEGER, parameter :: ny = 1080
     REAL :: lons(nx, ny), lats(nx, ny)
+    REAL :: dx, dy, tlat, tlon
 
     !Gemini
     integer, parameter :: N = nx*ny
@@ -20,48 +23,40 @@ program test_global_map
     allocate(tree_nodes(N))
     allocate(indices(N))
 
-    ! 1. Mock up your nonuniform grid data
-    ! Replace this loop with your actual file parsing/data stream ingestion
-    !do i = 1, N
-    !    ! Simulating random global points
-    !    call random_number(query_lat) ! 0 to 1
-    !    call random_number(query_lon) ! 0 to 1
-    !    tree_nodes(i)%lat_lon(1) = (query_lat * 180.0) - 90.0   ! -90 to 90
-    !    tree_nodes(i)%lat_lon(2) = (query_lon * 360.0) - 180.0  ! -180 to 180
-    !    
-    !    ! Pre-calculate 3D spatial points
-    !    call lat_lon_to_3d(tree_nodes(i)%lat_lon(1), tree_nodes(i)%lat_lon(2), tree_nodes(i)%coord)
-    !    
-    !    tree_nodes(i)%payload%grid_id = i
-    !    tree_nodes(i)%payload%value = 15.4 * i ! Sample payload data
-    !    
-    !    indices(i) = i ! Seed the indexing array
-    !end do
+    ! 1. Make up your nonuniform grid data
     ! RG
-    OPEN(FILE='rtofsll', FORM='UNFORMATTED', STATUS='OLD', unit=10)
-    READ (10) lons
-    READ (10) lats
+    !OPEN(FILE='rtofsll', FORM='UNFORMATTED', STATUS='OLD', unit=10)
+    !READ (10) lons
+    !READ (10) lats
     k = 0
+    dx = 360./nx
+    dy = 180./ny
     DO j = 1, ny
     DO i = 1, nx
-      k = k + 1
-      tree_nodes(k)%lat_lon(1) = lats(i,j)
-      tree_nodes(k)%lat_lon(2) = lons(i,j)
-      indices(k) = k
+      tlat = -90.+j*dy
+      if (tlat < -40 .or. tlat > 40) then
+        k = k + 1
+        !tree_nodes(k)%lat_lon(1) = lats(i,j)
+        !tree_nodes(k)%lat_lon(2) = lons(i,j)
+        tree_nodes(k)%lat_lon(1) = tlat
+        tree_nodes(k)%lat_lon(2) = i*dx
+        indices(k) = k
 
-      ! Pre-calculate 3D spatial points
-      call lat_lon_to_3d(tree_nodes(k)%lat_lon(1), tree_nodes(k)%lat_lon(2), tree_nodes(k)%coord)
+        ! Pre-calculate 3D spatial points
+        call lat_lon_to_3d(tree_nodes(k)%lat_lon(1), tree_nodes(k)%lat_lon(2), tree_nodes(k)%coord)
      
-      tree_nodes(k)%payload%grid_id = k
-      tree_nodes(k)%payload%value = 15.4 * k ! Sample payload data
+        tree_nodes(k)%payload%grid_id = k
+        tree_nodes(k)%payload%value = 15.4 * k ! Sample payload data
+      endif
     ENDDO
     ENDDO
+    PRINT *,'points on tree ',k
     !END RG
 
 
     ! 2. Build the Tree
     print *, "Building K-D Tree for RTOFS points..."
-    root_node = build_global_kd_tree(tree_nodes, indices, 1, N, 0)
+    root_node = build_global_kd_tree(tree_nodes, indices, 1, k, 0)
     print *, "Tree structural build finished! Root node is index: ", root_node
 
     ! 3. Search Example
