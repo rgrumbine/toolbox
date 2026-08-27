24f7462779beac37150102c3cad499c6c5a2d6e2
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2026-08-27 12:08:00 +0000

    towards 6 week forecasts

--- gefs/forecast6.py
+++ gefs/forecast6.py
@@ -38,7 +38,7 @@ tracemalloc.start()
 dt      = datetime.timedelta(1)
 nx      = 1536
 ny      =  768
-nlayer  =   26
+nlayer  =   20
 
 # Get memory metrics: (current, peak)
 Xavg   = np.zeros((ny, nx, nlayer), dtype=np.float32)
@@ -47,7 +47,7 @@ Xdata  = np.zeros((1, ny, nx, nlayer), dtype=np.float32)
 tag   = datetime.datetime(1994,1,4)
 
 # for climatology -- epoch has the class
-atm = climate2()
+atm = climate_trim()
 #debug: print("atm.epoch ",atm.x[0].epoch, flush = True)
 start = atm.x[0].epoch
 
@@ -64,49 +64,40 @@ print(f"Peak memory usage: {peak / 10**6} Mb", flush=True)
 
 # RG: In general this will be the GDAS file
 flx = nc.Dataset('thinned/week2.'+tag.strftime("%Y%m%d")+'.nc')
-Xdata[0,:,:,0] = flx.variables['ICETK'][:,:]
-Xdata[0,:,:,1] = flx.variables['ICEC'][:,:]
-Xdata[0,:,:,2] = flx.variables['SST'][:,:]
-Xdata[0,:,:,3] = flx.variables['USWRF'][:,:]
-Xdata[0,:,:,4] = flx.variables['TMPs'][:,:]
-Xdata[0,:,:,5] = flx.variables['TMP2m'][:,:]
-Xdata[0,:,:,6] = flx.variables['SPFH2m'][:,:]
-Xdata[0,:,:,7] = flx.variables['U10m'][:,:]
-Xdata[0,:,:,8] = flx.variables['V10m'][:,:]
-Xdata[0,:,:,9] = flx.variables['SHTFL'][:,:]
-Xdata[0,:,:,10] = flx.variables['LHTFL'][:,:]
-Xdata[0,:,:,11] = flx.variables['PWAT'][:,:]
-Xdata[0,:,:,12] = flx.variables['LAND'][:,:]
+Xdata[0,:,:,0] = flx.variables['ICEC'][:,:]
+Xdata[0,:,:,1] = flx.variables['SST'][:,:]
+Xdata[0,:,:,2] = flx.variables['TMPs'][:,:]
+Xdata[0,:,:,3] = flx.variables['TMP2m'][:,:]
+Xdata[0,:,:,4] = flx.variables['SPFH2m'][:,:]
+Xdata[0,:,:,5] = flx.variables['SHTFL'][:,:]
+Xdata[0,:,:,6] = flx.variables['LHTFL'][:,:]
+Xdata[0,:,:,7] = flx.variables['PWAT'][:,:]
+Xdata[0,:,:,8] = flx.variables['LAND'][:,:]
 
-Xdata[0,:,:,13] = flx.variables['PRMSL'][:,:]
-Xdata[0,:,:,14] = flx.variables['z1mb'][:,:]
-Xdata[0,:,:,15] = flx.variables['z10mb'][:,:]
-Xdata[0,:,:,16] = flx.variables['z200mb'][:,:]
-Xdata[0,:,:,17] = flx.variables['z500mb'][:,:]
-Xdata[0,:,:,18] = flx.variables['z700mb'][:,:]
-Xdata[0,:,:,19] = flx.variables['z850mb'][:,:]
+Xdata[0,:,:,9] = flx.variables['PRMSL'][:,:]
+Xdata[0,:,:,10] = flx.variables['z200mb'][:,:]
+Xdata[0,:,:,11] = flx.variables['z500mb'][:,:]
+Xdata[0,:,:,12] = flx.variables['z700mb'][:,:]
+Xdata[0,:,:,13] = flx.variables['z850mb'][:,:]
 flx.close()
 
 # RG: Note that start should be epoch for forecasting, 19940101
 # RG: note that this should be 365.2422
-Xdata[0,:,:,20] = cos( (tag-start)/dt * 2.*pi/365.25)
-Xdata[0,:,:,21] = sin( (tag-start)/dt * 2.*pi/365.25)
-Xdata[0,:,:,22] = cos(2*(tag-start)/dt * 2.*pi/365.25)
-Xdata[0,:,:,23] = sin(2*(tag-start)/dt * 2.*pi/365.25)
-Xdata[0,:,:,24] = cos(3*(tag-start)/dt * 2.*pi/365.25)
-Xdata[0,:,:,25] = sin(3*(tag-start)/dt * 2.*pi/365.25)
+Xdata[0,:,:,14] = cos( (tag-start)/dt * 2.*pi/365.2422)
+Xdata[0,:,:,15] = sin( (tag-start)/dt * 2.*pi/365.2422)
+Xdata[0,:,:,16] = cos(2*(tag-start)/dt * 2.*pi/365.2422)
+Xdata[0,:,:,17] = sin(2*(tag-start)/dt * 2.*pi/365.2422)
+Xdata[0,:,:,18] = cos(3*(tag-start)/dt * 2.*pi/365.2422)
+Xdata[0,:,:,19] = sin(3*(tag-start)/dt * 2.*pi/365.2422)
 
 # Remove climatology so as to have anomalies 
 Xdata -= Xavg
 
 # hard-wired scaling:
-#scale = [15, 1.2, 20, 600, 3800, 3100, 2225,  750, 500, 350, 300, 1, 1, 1, 1, 1, 1]
-scale  = [19, 1, 20, 600, 36, 25, 1.5e-2, 10, 10, 500, 500, 45, 2.e-4,
-           5000, 3400, 2500,  780, 530, 390, 340, 1, 1, 1, 1, 1, 1 ]  
-
+scale =  [1, 20, 36, 25, 2.e-2, 500, 600, 50, 2.e-4,
+        5000,  750, 500, 380, 330, 1, 1, 1, 1, 1, 1]
 for l in range(0,nlayer):
     Xdata[0,:,:,l] /= scale[l]
-
 
 #debug: sys.exit(0)
 
@@ -126,6 +117,8 @@ else:
 # make a forecast
 #debug: print("Xdata shape:",Xdata.shape, flush=True)
 Xpred = unet.predict(Xdata)
+#debug: 
+print("Xpred shape:",Xpred.shape, flush=True)
 
 tmp = time.time()
 print('time after making forecast ', tmp-tstart)
@@ -133,7 +126,6 @@ current, peak = tracemalloc.get_traced_memory()
 print(f"After making prediction memory usage: {current / 10**6} Mb")
 print(f"Peak memory usage: {peak / 10**6} Mb", flush=True)
 
-#debug: print("Xpred shape:",Xpred.shape, flush=True)
 #debug:
 print("predicted scaled",Xpred.max(), Xpred.min() , flush=True)
 #debug: sys.exit(0)
